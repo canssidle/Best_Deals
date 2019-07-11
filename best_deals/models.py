4bada4f95be939d06576f70a0b3c14f18ebb71a9
canssidle <canssidlewairimu@gmail.com>
2019-07-11 14:48:44 +0300

    create models

--- best_deals/models.py
+++ best_deals/models.py
@@ -1,26 +1,26 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from tinymce.models import HTMLField
+from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
-# class Profile(models.Model):
-#     avatar = models.ImageField(upload_to='avatars/')
-#     description = HTMLField()
-#     username = models.ForeignKey(User,on_delete=models.CASCADE)
-#     name =models.CharField(max_length=100)
-#     email = models.EmailField()
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='avatars/')
+    description = HTMLField()
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
 
-#     def __str__(self):
-#         return self.username
+    def __str__(self):
+        return self.username
 
 
-# class Business(models.Model):
-#     logo = models.ImageField(upload_to='businesslogo/')
-#     description = HTMLField()
-#     owner = models.ForeignKey(User,on_delete=models.CASCADE)
-#     name =models.CharField(max_length=100)
-#     email = models.EmailField()
-#     address =models.CharField(max_length=100)
-#     contact = models.IntegerField()
+class Business(models.Model):
+    logo = models.ImageField(upload_to='businesslogo/')
+    description = HTMLField()
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+    address =models.CharField(max_length=100)
+    contact = models.IntegerField()
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
